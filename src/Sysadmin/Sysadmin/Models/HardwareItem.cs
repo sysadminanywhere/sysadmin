f0442f3e7aba1373e0e98c10d3bf307c0db85c4a
sysadminanywhere <info@sysadminanywhere.com>
2022-05-13 14:27:09 +0300

    Hardware

---
+++ src/Sysadmin/Sysadmin/Models/HardwareItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysAdmin.Models
+{
+    public class HardwareItem
+    {
+
+        public string Name { get; set; }
+        public object Value { get; set; }
+    }
+}
--- src/Sysadmin/Sysadmin/Views/Computers/Management/HardwarePage.xaml.cs
+++ src/Sysadmin/Sysadmin/Views/Computers/Management/HardwarePage.xaml.cs
@@ -5,12 +5,16 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Sysadmin.WMI.Models.Hardware;
 using SysAdmin.ActiveDirectory.Models;
+using SysAdmin.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.RegularExpressions;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
@@ -24,6 +28,8 @@ namespace SysAdmin.Views.Computers.Management
     /// </summary>
     public sealed partial class HardwarePage : Page
     {
+
+        public HardwareViewModel ViewModel { get; } = new HardwareViewModel();
 
         public ComputerEntry Computer { get; set; }
 
@@ -42,9 +48,104 @@ namespace SysAdmin.Views.Computers.Management
             }
         }
 
-        private void ListBoxItem_Tapped(object sender, TappedRoutedEventArgs e)
+        private async void ListBoxItem_Tapped(object sender, TappedRoutedEventArgs e)
         {
+            ListBoxItem item = (ListBoxItem)sender;
 
+            switch (item.Tag)
+            {
+                case "computersystem":
+                    GetHardwares(await ViewModel.ComputerSystem(Computer));
+                    break;
+                case "bios":
+                    GetHardwares(await ViewModel.BIOS(Computer));
+                    break;
+                case "baseboard":
+                    GetHardwares(await ViewModel.BaseBoard(Computer));
+                    break;
+                case "diskdrive":
+                    GetHardwares(await ViewModel.DiskDrive(Computer));
+                    break;
+                case "operatingsystem":
+                    GetHardwares(await ViewModel.OperatingSystem(Computer));
+                    break;
+                case "diskpartition":
+                    GetHardwares(await ViewModel.DiskPartition(Computer));
+                    break;
+                case "processor":
+                    GetHardwares(await ViewModel.Processor(Computer));
+                    break;
+                case "videocontroller":
+                    GetHardwares(await ViewModel.VideoController(Computer));
+                    break;
+                case "physicalmemory":
+                    GetHardwares(await ViewModel.PhysicalMemory(Computer));
+                    break;
+                case "logicaldisk":
+                    GetHardwares(await ViewModel.LogicalDisk(Computer));
+                    break;
+            }
         }
+
+        private void GetHardwares(object hardware)
+        {
+            if (hardware.GetType().Name.StartsWith("List"))
+            {
+                comboBox.Items.Clear();
+
+                List<IHardware> list = new List<IHardware>((IEnumerable<IHardware>)hardware);
+
+                foreach (IHardware item in list)
+                {
+                    comboBox.Items.Add(item);
+                }
+
+                comboBox.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                comboBox.Visibility = Visibility.Collapsed;
+
+                ShowProperties(hardware);
+            }
+        }
+
+        private void ShowProperties(object hardware)
+        {
+            if (hardware != null)
+            {
+                foreach (PropertyInfo propertyInfo in hardware.GetType().GetProperties())
+                {
+                    string text = String.Empty;
+                    var value = propertyInfo.GetValue(hardware, null);
+
+                    if (value != null)
+                    {
+                        if (value is List<string>)
+                        {
+                            List<string> items = (List<string>)value;
+                            if (items.Count > 0)
+                                text = items.Select(a => a.ToString()).Aggregate((i, j) => i + ", " + j);
+                            else
+                                text = string.Empty;
+                        }
+                        else
+                        {
+                            text = value.ToString();
+                        }
+                    }
+
+                    string name = Regex.Replace(propertyInfo.Name, @"((?<=\p{Ll})\p{Lu})|((?!\A)\p{Lu}(?>\p{Ll}))", " $0");
+
+                    ViewModel.Items.Add(new Models.HardwareItem() { Name = name, Value = text });
+                }
+            }
+        }
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ShowProperties(comboBox.SelectedItem);
+        }
+
     }
 }
