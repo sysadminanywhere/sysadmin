58d2077290c8294b93bc4cd6d2458d2f388ae83c
sysadminanywhere <info@sysadminanywhere.com>
2023-06-27 13:15:56 +0300

    fixes

--- src/SysadminUI/Sysadmin/ViewModels/ComputersViewModel.cs
+++ src/SysadminUI/Sysadmin/ViewModels/ComputersViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using Sysadmin.Models;
+using SysAdmin.ActiveDirectory.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Windows.Media;
 using Wpf.Ui.Common.Interfaces;
 
@@ -13,6 +15,8 @@ namespace Sysadmin.ViewModels
 
         //[ObservableProperty]
         //private IEnumerable<DataColor> _colors;
+
+        public ObservableCollection<ComputerEntry> Computers { get; private set; } = new ObservableCollection<ComputerEntry>();
 
         public void OnNavigatedTo()
         {
