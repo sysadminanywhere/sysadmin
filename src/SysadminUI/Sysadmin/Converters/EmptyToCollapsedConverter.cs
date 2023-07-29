6549d725a85a69bf0d27113e517958f01407e0c1
sysadminanywhere <info@sysadminanywhere.com>
2023-07-29 16:51:13 +0300

    Login page

---
+++ src/SysadminUI/Sysadmin/Converters/EmptyToCollapsedConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SysAdmin.Converters
+{
+    public class EmptyToCollapsedConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return Visibility.Collapsed;
+
+            if (value is string)
+                if (string.IsNullOrEmpty(value.ToString()))
+                    return Visibility.Collapsed;
+
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
