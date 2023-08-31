e995a635ea055fb87e8e3aaf6078127ed2da28a2
sysadminanywhere <info@sysadminanywhere.com>
2023-08-31 20:21:17 +0300

    tree

--- src/SysadminUI/Sysadmin/Controls/DirectoryControl.xaml.cs
+++ src/SysadminUI/Sysadmin/Controls/DirectoryControl.xaml.cs
@@ -11,43 +11,30 @@ namespace Sysadmin.Controls
     /// <summary>
     /// Interaction logic for DirectoryControl.xaml
     /// </summary>
-    public partial class DirectoryControl : UserControl, INotifyPropertyChanged
+    public partial class DirectoryControl : UserControl
     {
 
-        private string distinguishedName { get; set; }
-
-        public string DistinguishedName
-        {
-            get
-            {
-                return distinguishedName;
-            }
-
-            set
-            {
-                if (value != distinguishedName)
-                {
-                    distinguishedName = value;
-                    NotifyPropertyChanged();
-                }
-            }
-        }
         public DirectoryControl()
         {
             this.InitializeComponent();
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
         private void btnSelect_Click(object sender, RoutedEventArgs e)
         {
             flyout.Show();
+            directoryTreeControl.Load();
         }
+
+        private void directoryTreeControl_SelectedItem(string DistinguishedName)
+        {
+            if (!string.IsNullOrEmpty(DistinguishedName))
+            {
+                distinguishedName.Text = DistinguishedName;
+            }
+
+            flyout.Hide();
+        }
+
     }
 
 }
